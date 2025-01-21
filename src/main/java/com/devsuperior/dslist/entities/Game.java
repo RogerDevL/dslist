18eb4a7a7c07ee8767c94583d323ec25713151a6
Roger Lemos <154293097+RogerDevL@users.noreply.github.com>
2025-01-21 14:49:17 -0300

    Game Seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -22,8 +22,13 @@ public class Game {
 	private Integer year;
 	private String genre;
 	private String platforms;
+	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	
@@ -32,7 +37,7 @@ public class Game {
 	}
 
 
-	public Game(Long id, String title, Integer year, String genre, String platforms, String imgUrl,
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		super();
 		this.id = id;
@@ -40,6 +45,7 @@ public class Game {
 		this.year = year;
 		this.genre = genre;
 		this.platforms = platforms;
+		this.score = score;	
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
@@ -93,6 +99,16 @@ public class Game {
 
 	public void setPlatforms(String platforms) {
 		this.platforms = platforms;
+	}
+	
+	
+	public Double getScore() {
+		return score;
+	}
+
+
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 
